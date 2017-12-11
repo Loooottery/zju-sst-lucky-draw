5d877397a85d4d5cdf965309f254bd1c75e1c1a5
Dxy <denight@qq.com>
2017-12-12 02:13:12 +0800

    换了个思路实现了基本的效果

--- index.js
+++ index.js
@@ -2,18 +2,12 @@ window.onload = function() {
   var bstart = document.getElementById('button-start')
 
   bstart.onclick = function() {
-    var count = 0
-
-    setInterval(function() {
-      setBoxNumber('box-1', count)
-      count++
-      count === 10 && (count = 0)
-    }, 800)
+    setBoxNumber('box', 9)
   }
 }
 
 function setBoxNumber(id, n) {
   var box = document.getElementById(id)
   var value = -1 * n * getComputedStyle(box).height.slice(0, -2)
-  box.children[0].style.transform = 'matrix(1,0,0,1,0,' + value + ')'
+  box.children[0].style.transform = 'matrix(1,0,0,1,60,' + value + ')'
 }
